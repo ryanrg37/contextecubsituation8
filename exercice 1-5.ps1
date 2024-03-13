fd465fb9065787e21633cf8a5ee1e2bddcfdd8dd
unknown <Administrateur@LOCAL.ANVERS.CUB.SIOPLC.FR>
2024-03-13 11:17:00 +0100

    exercice1-5.ps1

---
+++ exercice 1-5.ps1
@@ -0,0 +1,19 @@
+﻿#=============================================================================================
+#NAME:exercice1-3.ps1
+#AUTHOR: Garcia Ryan 
+#DATE: 13/03/2024
+#
+#Version 1.0
+#COMMENTS: liste les processus "SVCHOST"
+#
+#=============================================================================================
+#fonction 
+function infoprocess ($processus)
+{
+
+Get-Process -Name *$processus* | Format-Table "Id","ProcessName","description" 
+}
+#demande a l'utilisateur de renter un processus
+$processus = read-host "entrez le nom d'un processuce"
+#donne le resultat de la fonction 
+infoprocess ($processus)

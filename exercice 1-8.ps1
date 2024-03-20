db6aef7ba400e506d5b50667b5aee0b2e74007b7
unknown <Administrateur@LOCAL.ANVERS.CUB.SIOPLC.FR>
2024-03-20 09:38:06 +0100

    exercice1-8.ps1

---
+++ exercice 1-8.ps1
@@ -0,0 +1,26 @@
+﻿#=============================================================================================
+#NAME:exercice1-3.ps1
+#AUTHOR: Garcia Ryan 
+#DATE: 20/03/2024
+#
+#Version 1.0
+#COMMENTS: liste les processus "SVCHOST"
+#
+#=============================================================================================
+#fonction
+function Get-ServicesByStatus {
+    param ($status)
+
+    Write-Host "Liste des services $status :"
+    $services = Get-Service | Where-Object { $_.Status -eq $status }
+    $services | Select-Object DisplayName, Description, Status
+}
+
+# Demande à l'utilisateur de saisir l'état des services qu'il souhaite afficher
+$status = Read-Host "Entrez l'état des services à afficher (Running ou Stopped)"
+
+# Appel de la fonction Get-ServicesByStatus pour obtenir les services selon l'état spécifié par l'utilisateur
+$services = Get-ServicesByStatus -status $status
+
+# Exportation des informations dans un fichier CSV
+$services | Export-Csv -Path "C:/service_$status.csv" -NoTypeInformation -Encoding utf8
